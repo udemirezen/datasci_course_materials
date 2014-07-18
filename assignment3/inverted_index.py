d859d34bb3412c83ede53b651b732df7d23d1a12
M.Umut DEMİREZEN <umut@didem.umutdemirezen.com>
2014-07-18 20:10:16 +0300

    1 tamam iki başladı

---
+++ assignment3/inverted_index.py
@@ -0,0 +1,38 @@
+import MapReduce
+import sys
+
+"""
+Inverted Index Python Script By Umut DEMiREZEN....
+"""
+
+mr = MapReduce.MapReduce()
+
+# =============================
+# Do not modify above this line
+
+def mapper(record):
+    # key: document identifier
+    # value: document contents
+    key = record[0]
+    value = record[1]
+    words = value.split()
+    for w in words:
+      mr.emit_intermediate(w, key)
+
+def reducer(key, list_of_values):
+    # key: word
+    # value: list of occurrence counts
+    total = []
+    for v in list_of_values:
+
+      if total.count(v) > 0:
+          total.remove(v)
+
+      total.append(v)
+    mr.emit((key, total))
+
+# Do not modify below this line
+# =============================
+if __name__ == '__main__':
+  inputdata = open(sys.argv[1])
+  mr.execute(inputdata, mapper, reducer)
